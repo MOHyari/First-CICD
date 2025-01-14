ea70ddb506b2834aedb2731537653e5eaa068c35
Mohammad Alhyari <mo.alhyari2000@gmail.com>
2025-01-14 18:02:48 +0300

    fixed minor errors

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,87 +1,90 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~>4.0"
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "~>4.0"
+      }
     }
-  }
-  backend "s3" {
-    key = "aws/ec2-deploy/terraform.tfstate"
-  }
+    backend "s3" {
+      key = "aws/ec2-deploy/terraform.tfstate"
+    }
+    
 }
 
 provider "aws" {
-  region = var.region"
+    region = var.region
+
 }
-resource "aws_instance" "servernode" {
-  ami                    = "ami-0e2c8caa4b6378d8c"
-  instance_type          = "t2.micro"
-  key_name               = aws_key_pair.deployer.key_name
-  vpc_security_group_ids = [aws_security_group.firstgroup.id]
-  iam_instance_profile   = aws_iam_instance_profile.ec2-profile.name
-  connection {
-    type        = "ssh"
-    host        = self.public_ip
-    user        = "ubuntu"
-    private_key = var.private_key
-    timeout     = "4m"
-  }
-  tags = {
-    "name" = "DeployVM"
-  }
+resource "aws_instance" "server" {
+    ami = "ami-0e2c8caa4b6378d8c"
+    instance_type = "t2.micro"
+    key_name = aws_key_pair.deployer.key_name
+    vpc_security_group_ids = [aws_security_group.firstgroup.id]
+    iam_instance_profile = aws_iam_instance_profile.ec2-profile.name
+    connection {
+        type = "ssh"
+        host = self.public_ip
+        user = "ubumtu"
+        private_key = var.private_key
+        timeout = "4m"
+    }
+    tags = {
+        "name" = "Deploy Virtual Machine"
+    }
+
 }
 resource "aws_iam_instance_profile" "ec2-profile" {
-  name = "ec2-profile"
-  role = "EC2-ECR-AUTH"
+    name = "ec2-profile"
+    role = "EC2-ECR-AUTH"
+
 }
 resource "aws_security_group" "firstgroup" {
-  egress = [
-    {
-      cidr_blocks      = ["0.0.0.0/0"]
-      description      = ""
-      from_port        = 0
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "-1"
-      security_groups  = []
-      self             = false
-      to_port          = 0
-    }
-  ]
-  ingress = [
-    {
-      cidr_blocks      = ["0.0.0.0/0", ]
-      description      = ""
-      from_port        = 22
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "tcp"
-      security_groups  = []
-      self             = false
-      to_port          = 22
-    },
-    {
-      cidr_blocks      = ["0.0.0.0/0", ]
-      description      = ""
-      from_port        = 80
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "tcp"
-      security_groups  = []
-      self             = false
-      to_port          = 80
-    }
-  ]
+    egress = [ 
+        {
+            cidr_blocks = ("0.0.0.0/0")
+            description = ""
+            from_port = 0 
+            ipv6_cidr_blocks = []
+            prefix_list = []
+            protocol = "-1"
+            security_groups = []
+            self = false
+            to_port = 0
 
-
-}
+        }
+    ]
+    ingress = [
+        {
+            cidr_blocks = ("0.0.0.0/0")
+            description = ""
+            from_port = 22 
+            ipv6_cidr_blocks = []
+            prefix_list = []
+            protocol = "tcp"
+            security_groups = []
+            self = false
+            to_port = 22
+        },
+        {
+           cidr_blocks = ("0.0.0.0/0")
+            description = ""
+            from_port = 80 
+            ipv6_cidr_blocks = []
+            prefix_list = []
+            protocol = "tcp"
+            security_groups = []
+            self = false
+            to_port = 80
+        }
+    ]
+} 
 
 resource "aws_key_pair" "deployer" {
-  key_name   = var.key_name
-  public_key = var.public_key
+    key_name = var.key_name
+    public_key = var.public_key
 }
 
 output "instance_public_ip" {
-  value     = aws_instance.servernode.public_ip
-  sensitive = true
+    value = aws.instance.server.public_ip
+    sensitive = true
 }
