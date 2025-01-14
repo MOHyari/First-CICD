babbc1c41c1aceb55bcf1f2dfc08e359f8ac9ac0
Mohammad AlHyari <87830847+MOHyari@users.noreply.github.com>
2025-01-14 18:39:02 +0300

    fix security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,45 +39,45 @@ resource "aws_iam_instance_profile" "ec2-profile" {
 
 }
 resource "aws_security_group" "firstgroup" {
-    egress = [ 
+    egress = [
         {
-            cidr_blocks = ("0.0.0.0/0")
-            description = ""
-            from_port = 0 
+            cidr_blocks = ["0.0.0.0/0"]
+            description = "Allow all outbound traffic"
+            from_port = 0
             ipv6_cidr_blocks = []
-            prefix_list = []
+            prefix_list_ids = []  # Add this line if you are using prefix lists
             protocol = "-1"
             security_groups = []
             self = false
             to_port = 0
-
         }
     ]
+
     ingress = [
         {
-            cidr_blocks = ("0.0.0.0/0")
-            description = ""
-            from_port = 22 
+            cidr_blocks = ["0.0.0.0/0"]
+            description = "Allow SSH"
+            from_port = 22
             ipv6_cidr_blocks = []
-            prefix_list = []
+            prefix_list_ids = []  # Add this line if you are using prefix lists
             protocol = "tcp"
             security_groups = []
             self = false
             to_port = 22
         },
         {
-           cidr_blocks = ("0.0.0.0/0")
-            description = ""
-            from_port = 80 
+            cidr_blocks = ["0.0.0.0/0"]
+            description = "Allow HTTP"
+            from_port = 80
             ipv6_cidr_blocks = []
-            prefix_list = []
+            prefix_list_ids = []  # Add this line if you are using prefix lists
             protocol = "tcp"
             security_groups = []
             self = false
             to_port = 80
         }
     ]
-} 
+}
 
 resource "aws_key_pair" "deployer" {
     key_name = var.key_name
