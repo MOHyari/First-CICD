ab5a61bde09572a57433d02f643e33d4ae8e75bd
Mohammad AlHyari <87830847+MOHyari@users.noreply.github.com>
2025-01-14 18:53:33 +0300

    fix variables for last step in pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,6 +13,9 @@ terraform {
 
 provider "aws" {
     region = var.region
+    access_key = var.aws_access_key_id
+    secret_key = var.aws_secret_access_key
+
 
 }
 resource "aws_instance" "server" {
