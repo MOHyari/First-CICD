d1861bd5d81fe0abecf5db392b256c5f53c1efe7
Mohammad AlHyari <87830847+MOHyari@users.noreply.github.com>
2025-01-14 18:42:53 +0300

    _ not .

--- terraform/main.tf
+++ terraform/main.tf
@@ -85,6 +85,6 @@ resource "aws_key_pair" "deployer" {
 }
 
 output "instance_public_ip" {
-    value = aws.instance.server.public_ip
+    value = aws_instance.server.public_ip
     sensitive = true
 }
