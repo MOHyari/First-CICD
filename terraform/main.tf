37f93710a8c0540be4883016290d2bd2fcdedf34
Mohammad AlHyari <87830847+MOHyari@users.noreply.github.com>
2025-01-19 20:18:18 +0300

    fix duplicate variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,3 @@
-variable "region" {
-    default = "us-east-1"
-}
-
-variable "public_key" {
-    description = "The public key for SSH access"
-}
-
-variable "private_key" {
-    description = "The private key for SSH access"
-}
-
-variable "key_name" {
-    description = "The name of the key pair"
-}
-
-variable "aws_access_key_id" {
-    description = "AWS Access Key ID"
-}
-
-variable "aws_secret_access_key" {
-    description = "AWS Secret Access Key"
-}
-
 terraform {
     required_providers {
         aws = {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,20 +1,23 @@
 variable "region" {
     default = "us-east-1"
 }
+
 variable "public_key" {
-  
+    description = "The public key for SSH access"
 }
+
 variable "private_key" {
-  
+    description = "The private key for SSH access"
 }
+
 variable "key_name" {
-  
+    description = "The name of the key pair"
 }
 
 variable "aws_access_key_id" {
-  description = "AWS Access Key ID"
+    description = "AWS Access Key ID"
 }
 
 variable "aws_secret_access_key" {
-  description = "AWS Secret Access Key"
+    description = "AWS Secret Access Key"
 }
