458330f5d50e7b20ba319f8b297fc265a54d8e0d
Mohammad AlHyari <87830847+MOHyari@users.noreply.github.com>
2025-01-19 20:16:20 +0300

    fixing syntax in multiple files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,58 +1,78 @@
+variable "region" {
+    default = "us-east-1"
+}
+
+variable "public_key" {
+    description = "The public key for SSH access"
+}
+
+variable "private_key" {
+    description = "The private key for SSH access"
+}
+
+variable "key_name" {
+    description = "The name of the key pair"
+}
+
+variable "aws_access_key_id" {
+    description = "AWS Access Key ID"
+}
+
+variable "aws_secret_access_key" {
+    description = "AWS Secret Access Key"
+}
+
 terraform {
     required_providers {
-      aws = {
-        source = "hashicorp/aws"
-        version = "~>4.0"
-      }
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~>4.0"
+        }
     }
     backend "s3" {
-      key = "aws/ec2-deploy/terraform.tfstate"
+        key = "aws/ec2-deploy/terraform.tfstate"
     }
-    
 }
 
 provider "aws" {
-    region = var.region
+    region     = var.region
     access_key = var.aws_access_key_id
     secret_key = var.aws_secret_access_key
-
-
 }
+
 resource "aws_instance" "server" {
-    ami = "ami-0e2c8caa4b6378d8c"
-    instance_type = "t2.micro"
-    key_name = aws_key_pair.deployer.key_name
+    ami                    = "ami-0e2c8caa4b6378d8c"
+    instance_type         = "t2.micro"
+    key_name              = aws_key_pair.deployer.key_name
     vpc_security_group_ids = [aws_security_group.firstgroup.id]
-    iam_instance_profile = aws_iam_instance_profile.ec2-profile.name
+    iam_instance_profile   = aws_iam_instance_profile.ec2-profile.name
+
     connection {
-        type = "ssh"
-        host = self.public_ip
-        user = "ubumtu"
+        type        = "ssh"
+        host        = self.public_ip
+        user        = "ubuntu"  # Corrected from "ubumtu"
         private_key = var.private_key
-        timeout = "4m"
+        timeout     = "4m"
     }
+
     tags = {
         "name" = "Deploy Virtual Machine"
     }
-
 }
+
 resource "aws_iam_instance_profile" "ec2-profile" {
     name = "ec2-profile"
     role = "EC2-ECR-AUTH"
-
 }
+
 resource "aws_security_group" "firstgroup" {
     egress = [
         {
             cidr_blocks = ["0.0.0.0/0"]
             description = "Allow all outbound traffic"
-            from_port = 0
-            ipv6_cidr_blocks = []
-            prefix_list_ids = []  # Add this line if you are using prefix lists
-            protocol = "-1"
-            security_groups = []
-            self = false
-            to_port = 0
+            from_port   = 0
+            to_port     = 0
+            protocol    = "-1"
         }
     ]
 
@@ -60,34 +80,26 @@ resource "aws_security_group" "firstgroup" {
         {
             cidr_blocks = ["0.0.0.0/0"]
             description = "Allow SSH"
-            from_port = 22
-            ipv6_cidr_blocks = []
-            prefix_list_ids = []  # Add this line if you are using prefix lists
-            protocol = "tcp"
-            security_groups = []
-            self = false
-            to_port = 22
+            from_port   = 22
+            to_port     = 22
+            protocol    = "tcp"
         },
         {
             cidr_blocks = ["0.0.0.0/0"]
             description = "Allow HTTP"
-            from_port = 80
-            ipv6_cidr_blocks = []
-            prefix_list_ids = []  # Add this line if you are using prefix lists
-            protocol = "tcp"
-            security_groups = []
-            self = false
-            to_port = 80
+            from_port   = 80
+            to_port     = 80
+            protocol    = "tcp"
         }
     ]
 }
 
 resource "aws_key_pair" "deployer" {
-    key_name = var.key_name
+    key_name   = var.key_name
     public_key = var.public_key
 }
 
 output "instance_public_ip" {
-    value = aws_instance.server.public_ip
+    value     = aws_instance.server.public_ip
     sensitive = true
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,32 +10,11 @@ variable "private_key" {
 variable "key_name" {
   
 }
+
 variable "aws_access_key_id" {
   description = "AWS Access Key ID"
-  type        = string
-  default     = ""
 }
 
 variable "aws_secret_access_key" {
   description = "AWS Secret Access Key"
-  type        = string
-  default     = ""
-}
-
-variable "tf_state_bucket_name" {
-  description = "S3 bucket name for Terraform state"
-  type        = string
-  default     = ""
-}
-
-variable "private_ssh_key" {
-  description = "Private SSH key for accessing instances"
-  type        = string
-  default     = ""
-}
-
-variable "public_ssh_key" {
-  description = "Public SSH key for accessing instances"
-  type        = string
-  default     = ""
 }
